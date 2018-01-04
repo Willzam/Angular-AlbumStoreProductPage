f3ab140e4389785167f34ea4410468a33433a1c7
Willzam <35114230+Willzam@users.noreply.github.com>
2018-01-04 15:21:53 -0600

    Section5 fix

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,12 +9,12 @@ import {ProductService} from '../product.service';
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  albuminfo: Album;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albuminfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
